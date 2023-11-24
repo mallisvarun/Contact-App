1d6e1f5b2597caf27b0b2ccc071ee3ecc56d258e
Varun Malleshwaram <84590524+mallisvarun@users.noreply.github.com>
2023-11-24 21:57:52 +0530

    24/11
    24/11
    use state props

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,11 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 const ContactList =(props)=>{
     console.log(props);
     const rendercontactlist = props.contacts.map(
         (contact) =>{
             return(
-                <div className="item">
-                    <div className="content">
-                        <div className="header">{contact.name}</div>
-                        <div>{contact.email}</div>
-                        <i className="trash alternate outline icon"></i>
-                    </div>
-                    
-                </div>
-                
+                <ContactCard contact={contact}></ContactCard>
             );
         });
 
